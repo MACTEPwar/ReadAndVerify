8ec61c3f8550750bf3189c46a1fab9ad34e64c0b
Виталий Шебаниц <mactepwar@outlook.com>
2018-10-14 06:43:37 +0300

    First commit

---
+++ ReadAndVerify/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadAndVerify
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Project> _projects = Project.GetProjects();
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            //Project.Create(new List<Project> {
+            //    new Project("Мой проект 2", DateTime.Parse("01.10.2018"), DateTime.Parse("10.10.2018")),
+            //    new Project("Мой проект 3", DateTime.Parse("30.10.2018"), DateTime.Parse("20.12.2018"))
+            //});
+            //_projects = Project.GetProjects();
+            //this.DataContext = _projects;
+            //this.Resources.Add("Projects", _projects);
+            //lstCars.ItemTemplate = new DataTemplate();
+            //DataTemplate asd = new DataTemplate();
+            lstCars.ItemsSource = _projects;
+            //List<Project> Projects = Project.GetProjects(); 
+            CreateDynamicGridView(); 
+        }
+
+        private void CreateDynamicGridView()
+        {
+            // Create a GridView  
+            //GridView grdView = new GridView();
+            //grdView.AllowsColumnReorder = true;
+            //grdView.ColumnHeaderToolTip = "Project";
+            //GridViewColumn nameColumn = new GridViewColumn();
+            //nameColumn.DisplayMemberBinding = new Binding("Title");
+            //nameColumn.Header = "Project Title";
+            //nameColumn.Width = 120;
+            //grdView.Columns.Add(nameColumn);
+            //ListView1.View = grdView;
+        }
+    }
+}
---
+++ ReadAndVerify/Project.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+
+namespace ReadAndVerify
+{
+    [Serializable]
+    public class Project
+    {
+        [XmlIgnore]
+        private static string pathToFile = @"..\..\Xml\Project.xml";
+
+        public string Title { get; set; }
+        [XmlElement("StartDate")]
+        public DateTime StartDateForProject { get; set; }
+        [XmlElement("FinishDate")]
+        public DateTime FinishDateForProject { get; set; }
+
+        public Project() { }
+
+        public Project(string title,DateTime startDate, DateTime finishDate)
+        {
+            Title = title;
+            StartDateForProject = startDate;
+            FinishDateForProject = finishDate;
+        }
+
+        public static List<Project> GetProjects()
+        {
+            XmlRootAttribute xRoot = new XmlRootAttribute();
+            xRoot.ElementName = "Projects";
+            xRoot.IsNullable = true;
+
+            XmlSerializer xSeriz = new XmlSerializer(typeof(Project[]),xRoot);
+            List<Project> projects = new List<Project>();
+            using (FileStream fs = new FileStream(pathToFile, FileMode.OpenOrCreate))
+            {
+                Project[] newprojects = (Project[])xSeriz.Deserialize(fs);
+                return newprojects.Cast<Project>().ToList();
+            }
+        }
+
+        public static void Create(List<Project> projects)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(pathToFile);
+            foreach (Project project in projects)
+            {
+                XmlNode xnode = doc.CreateNode(XmlNodeType.Element, "Project", null);
+                XmlSerializer xSeriz = new XmlSerializer(typeof(Project));
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "");
+                XmlWriterSettings writtersetting = new XmlWriterSettings();
+                writtersetting.OmitXmlDeclaration = true;
+                StringWriter sw = new StringWriter();
+                using (XmlWriter xmlwriter = System.Xml.XmlWriter.Create(sw, writtersetting))
+                {
+                    xSeriz.Serialize(xmlwriter, project, ns);
+                }
+                byte[] symbol = Encoding.GetEncoding(1251).GetBytes(sw.ToString()); //Получаем байт символа в кодировке 1251
+                Console.WriteLine(symbol[0]);
+                string binary = Convert.ToString(symbol[0], 2);
+                symbol[0] = Convert.ToByte(binary, 2); //перевели обратно в десятичную
+                string r = Encoding.GetEncoding(1251).GetString(symbol); //Преобразовали обратно в строку
+                xnode.InnerXml = r;
+                XmlNode bindxnode = xnode.SelectSingleNode("Project");
+                foreach (XmlNode ch in xnode)
+                {
+                    doc.DocumentElement.AppendChild(ch);
+                }
+            }
+            doc.Save(pathToFile);
+        }
+
+        public override string ToString()
+        {
+            return "20";
+        }
+
+    }
+}
